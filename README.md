da0f851508662faa28742afa0706cd64f39b099c
卫志龙 <weizhilong@lppz.com>
2023-10-12 21:49:58 +0800

    添加springboot

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 
 # video-center
 private-room is a 
+
+# 版本0.0.1 
+## 完成基本前端框架，后端框架
+
+前端：vue3+ts+pina+vue-router \
+后端：
---
+++ VideoCenter/src/test/java/org/wzl/videocenter/VideoCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wzl.videocenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
